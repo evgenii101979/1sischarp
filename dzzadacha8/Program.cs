7ee15beca254c707f2be7f71211234e9a2bb005b
evgenii <evgenii.lyabov@mail.ru>
2022-09-21 21:16:47 +0300

    add dzzadacha8+++

---
+++ dzzadacha8/Program.cs
@@ -0,0 +1,30 @@
+﻿//    Задача 8: Напишите программу, которая на вход принимает число (N), 
+//    а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("введите целое число: ");
+int N = int.Parse(Console.ReadLine());
+int count = N;
+while(count = 1);
+{
+    if ((N % 2) == 0);
+    
+    Console.WriteLine();
+}
+
+//while (num = 1)
+//{
+  //  
+  //  if (result != 1)
+  //  {
+  //      Console.WriteLine(num - 1);
+  //      num++;
+  //  }
+  //  else
+  //  {
+  //      num++;
+  //  }
+//}
